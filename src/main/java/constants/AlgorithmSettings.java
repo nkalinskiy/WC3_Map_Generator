403432898f12a3a828185359d583ede7a201123f
kn878 <kalinskiy98@gmail.com>
2019-11-24 15:52:40 +0300

    Changed algorithm settings

--- src/main/java/constants/AlgorithmSettings.java
+++ src/main/java/constants/AlgorithmSettings.java
@@ -2,7 +2,7 @@ package constants;
 
 public final class AlgorithmSettings {
     public static final String ALGORITHM_NAME = "SMS-EMOA";
-    public static final int MAX_EVALUATIONS = 50000;
+    public static final int MAX_EVALUATIONS = 30000;
     public static final int POPULATION_SIZE = 10;
     public static final String CHECKPOINT_FILE = "test.state";
     public static final int CHECKPOINT_FREQUENCY = 1000;
--- src/main/java/constants/MapConfig.java
+++ src/main/java/constants/MapConfig.java
@@ -3,11 +3,11 @@ package constants;
 public final class MapConfig {
     public final static int N_BASES = 2;
     public final static int N_WOODS = 4;
-    public final static int N_GOLD_MINES = 4;
+    public final static int N_GOLD_MINES = 2;
     public final static int N_HEROES_SHOPS = 1;
     public final static int N_ITEMS_SHOPS = 1;
     public final static int N_NEUTRAL_CREEPS_CAMPS = 4;
-    public final static int N_IMPASSABLE_AREAS = 3;
+    public final static int N_IMPASSABLE_AREAS = 4;
     public final static double MAP_WIDTH = 64;
     public final static double MAP_HEIGHT = 64;
     //How many wood cells are generated around the genotype one
